3a11d790fb3fb67c09ae51f3ae04ee91d0c22e85
Jane Engineer <jane@microsoft.com>
2025-03-06 18:18:46 +0530

    add RULES, correct audio format, ignore mp3 and wav files

---
+++ RULES.md
@@ -0,0 +1,7 @@
+# Rules to be followed for text input
+
+-   use `comma`, `colon` for break/gap in the speech
+-   never use `*` for multiplication (write the word `times` instead)
+-   use `+`, `-` for the word plus or minus
+-   `ax` results in axe, use `"A" x` instead
+-   use `^` for "to the power of"
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import config from "./config/env.js";
 import fs from "fs";
 
 const textFile = "input.txt";
-const audioFile = "Preview.mp3";
+const audioFile = "Preview.wav";
 
 const speechConfig = SpeechConfig.fromSubscription(
     config.speechKey,
@@ -31,7 +31,7 @@ fs.readFile(textFile, "utf8", function (err, text) {
 
     synthesizer.speakTextAsync(
         text,
-        function (result) {
+        (result) => {
             if (result.reason === ResultReason.SynthesizingAudioCompleted) {
                 console.log("synthesis finished.");
             } else {
@@ -44,7 +44,7 @@ fs.readFile(textFile, "utf8", function (err, text) {
             synthesizer.close();
             synthesizer = null;
         },
-        function (err) {
+        (err) => {
             console.trace("err - " + err);
             synthesizer.close();
             synthesizer = null;
